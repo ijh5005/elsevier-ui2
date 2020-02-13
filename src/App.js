153e39340ecb20b2a72ff69a48351f0e26df6d09
Isaiah Harrison <ijh5005@outlook.com>
2020-02-12 23:13:53 -0500

    first change

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from "axios";
+
+import Table from "./components/Table";
+// axios.defaults = "http://localhost:8080";
+// console.log(axios.defaults)
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
+  timeout: 60 * 1000 * 4
+});
+
+// const getInitialResults = async () => {
+//   return await axiosInstance({
+//     method: "get",
+//     url: "/",
+//     timeout: 60 * 4 * 1000
+//   });
+// }
+
+const filter = (filters, resourceText) => {
+  axiosInstance.get("/filter", {
+    filters,
+	  resourceText
+  })
+}
+
+const App = () => {
+  
+  const [list, setList] = useState([]);
+  const [resources, setResources] = useState([]);
+  const heads = ["Clinical Status", "Date Recorded", "Verification Status", "Resource"]
+
+  useEffect(() => {
+    axiosInstance.get("/").then(results => {
+      const {
+        data
+      } = results;
+      const list = data.data.map(d => ({
+        clinicalStatus: d.resource.clinicalStatus,
+        dateRecorded: d.resource.dateRecorded,
+        verificationStatus: d.resource.verificationStatus,
+        resource: d.resource.code.text
+      }));
+      setList(list)
+      setResources(data.data.map(d => d.resource.code.text))
+    })
+  }, []);
+  return (
+    <div className="App">
+      <Table list={list} heads={heads} resources={resources}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import "./css/bootstrap.min.css";
+import "./css/main.css";
+import "./css/util.css";
+
+function Table(props) {
+
+  const [] = useState("")
+
+  const [clinicalStatus, setClinicalStatus] = useState();
+  const [dateRecorded, setDateRecorded] = useState();
+  const [verificationStatus, setVerificationStatus] = useState();
+  const [resource, setResource] = useState();
+
+  const {
+    heads,
+    list,
+    resources
+  } = props;
+
+  return (
+    <div class="limiter">
+
+      <div>
+
+        <select value={clinicalStatus} onChange={e => {
+          e.preventDefault();
+          setClinicalStatus(e.target.value)
+        }}>
+          <option value=""></option>
+          <option value="resolved">resolved</option>
+          <option value="active">active</option>
+        </select>
+
+        <select value={verificationStatus} onChange={e => {
+          e.preventDefault();
+          setVerificationStatus(e.target.value)
+        }}>
+          <option value=""></option>
+          <option value="resolved">confirmed</option>
+          <option value="active">differential</option>
+        </select>
+
+        <select value={resource} onChange={e => {
+          e.preventDefault();
+          setResource(e.target.value)
+        }}>
+          <option value=""></option>
+          {resources.map((data, i) => <option key={i} value={`${data}`}>{data}</option>)}
+        </select>
+
+      </div>
+
+      <div class="container-table100">
+        <div class="wrap-table100">
+
+          <div class="table100 ver1 m-b-110">
+            <div class="table100-head">
+              <table>
+                <thead>
+                  <tr class="row100 head">
+                    {heads.map((data, i) => <th key={i} class="cell100 column1">{data}</th>)}
+                  </tr>
+                </thead>
+              </table>
+            </div>
+
+            <div class="table100-body js-pscroll">
+              <table>
+                <tbody>
+                  {list.map((data, i) => (<tr key={i} class="row100 body">
+                    <td class="cell100 column1">{data.clinicalStatus}</td>
+                    <td class="cell100 column3">{data.dateRecorded}</td>
+                    <td class="cell100 column4">{data.verificationStatus}</td>
+                    <td class="cell100 column5">{data.resource}</td>
+                  </tr>))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
